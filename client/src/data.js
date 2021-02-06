606ecd8b4a2ce68a1d8f1d4aad183ff90547bd06
JoeZ <josephzasiebida@gmail.com>
2021-02-06 09:55:55 -0600

    'init'

---
+++ client/src/data.js
@@ -0,0 +1,71 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Nike Slim Shirt',
+            category: 'Shirt',
+            image: '/images/p1.jpg',
+            brand: 'Nike',
+            price: 120,
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id: '2',
+            name: 'Adidas fit Shirt',
+            category: 'Shirt',
+            image: '/images/p2.jpg',
+            brand: 'Nike',
+            price: 120,
+            rating: 4.0,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id: '3',
+            name: 'lacoste free Shirt',
+            category: 'Shirt',
+            image: '/images/p3.jpg',
+            brand: 'Nike',
+            price: 220,
+            rating: 4.8,
+            numReviews: 17,
+            description: 'high quality product'
+        },
+        {
+            _id: '4',
+            name: 'Nike Slim Shirt',
+            category: 'Pants',
+            image: '/images/p4.jpg',
+            brand: 'Nike',
+            price: 130,
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id: '5',
+            name: 'Puma Slim Shirt',
+            category: 'Shirt',
+            image: '/images/p5.jpg',
+            brand: 'Nike',
+            price: 120,
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product',
+        },
+        {
+            _id: '6',
+            name: 'Adidas Fit Pants',
+            category: 'Shirt',
+            image: '/images/p6.jpg',
+            brand: 'Nike',
+            price: 'Adidas',
+            rating: 4.5,
+            numReviews: 15,
+            description: 'high quality product'
+        }
+    ]
+}
+export default data
