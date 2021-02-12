3ffae16a260f890fd036a15143529e805bf523c3
JoeZ <josephzasiebida@gmail.com>
2021-02-11 20:29:49 -0600

    'mongodb'

--- backend/server.js
+++ backend/server.js
@@ -10,40 +10,26 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useUnifiedTopology: true,
-            useNesUrlParser: true,
-            useCreateIndex: true
-        })
-    } catch (error) {
-        console.error(`Error: ${error.message}`)
-        process.exit(1)
-    }
-}
-connectDB()
-
-
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+});
 app.use('/api/users', userRouter);
-app.use('/api/products', productRouter)
+app.use('/api/products', productRouter);
+app.use('/api/orders', orderRouter);
 app.get('/api/config/paypal', (req, res) => {
-    res.send(process.env.PAYPAL_CLIENT_ID || 'sb')
-})
-app.use('/api/orders', orderRouter)
-
-
-
+    res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
+});
 app.get('/', (req, res) => {
-    res.send('Server is ready')
-})
+    res.send('Server is ready');
+});
 
 app.use((err, req, res, next) => {
-    res.status(500).send({ message: err.message })
-})
+    res.status(500).send({ message: err.message });
+});
 
 const port = process.env.PORT || 5000;
-
 app.listen(port, () => {
-    console.log(`server at http://localhost:${port}`)
-})
+    console.log(`Serve at http://localhost:${port}`);
+});
